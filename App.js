8e5c0957cd9add8e10dc9f8a5915d78e80a2e958
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2021-07-20 17:21:29 -0300

    Aula 2.2

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView } from 'react-native';
 
 import Cesta from './src/telas/Cesta';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView>
+      <StatusBar />
       <Cesta />
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,6 +1,30 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Image, Dimensions, Text } from 'react-native';
+
+import topo from '../../assets/topo.png';
+
+const width = Dimensions.get('screen').width;
 
 export default function Cesta() {
-  return <Text>Cesta</Text>
+  return <>
+    <Image source={topo} style={estilos.topo} />
+    <Text style={estilos.titulo}>Detalhe da cesta</Text>
+  </>
 }
+
+const estilos = StyleSheet.create({
+  topo: {
+    width: "100%",
+    height: 578 / 768 * width,
+  },
+  titulo: {
+    width: "100%",
+    position: "absolute",
+    textAlign: "center",
+    fontSize: 16,
+    lineHeight: 26,
+    color: "white",
+    fontWeight: "bold",
+    padding: 16,
+  }
+});
