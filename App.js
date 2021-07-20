8a8b8f4860ff7cb66a977c4e5615db423768d4b0
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2021-07-20 15:02:02 -0300

    Aula 2.1

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Cesta from './src/telas/Cesta';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Alura!</Text>
+      <Cesta />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/telas/Cesta.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function Cesta() {
+  return <Text>Cesta</Text>
+}
