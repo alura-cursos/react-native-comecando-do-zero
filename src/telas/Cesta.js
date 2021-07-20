e64849852ee2aae9136d50f46258ee08fe51772c
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2021-07-20 18:46:30 -0300

    Aula 2.3

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, Image, Dimensions, Text } from 'react-native';
+import { StyleSheet, Image, Dimensions, Text, View } from 'react-native';
 
 import topo from '../../assets/topo.png';
+import logo from '../../assets/logo.png';
 
 const width = Dimensions.get('screen').width;
 
@@ -9,6 +10,16 @@ export default function Cesta() {
   return <>
     <Image source={topo} style={estilos.topo} />
     <Text style={estilos.titulo}>Detalhe da cesta</Text>
+
+    <View style={estilos.cesta}>
+      <Text style={estilos.nome}>Cesta de Verduras</Text>
+      <View style={estilos.fazenda}>
+        <Image source={logo} style={estilos.imagemFazenda} />
+        <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+      </View>
+      <Text style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Text>
+      <Text style={estilos.preco}>R$ 40,00</Text>
+    </View>
   </>
 }
 
@@ -26,5 +37,40 @@ const estilos = StyleSheet.create({
     color: "white",
     fontWeight: "bold",
     padding: 16,
-  }
+  },
+  cesta: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  nome: {
+    color: "#464646",
+    fontSize: 26,
+    lineHeight: 42,
+    fontWeight: "bold",
+  },
+  fazenda: {
+    flexDirection: "row",
+    paddingVertical: 12,
+  },
+  imagemFazenda: {
+    width: 32,
+    height: 32,
+  },
+  nomeFazenda: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 12,
+  },
+  descricao: {
+    color: "#A3A3A3",
+    fontSize: 16,
+    lineHeight: 26,
+  },
+  preco: {
+    color: "#2A9F85",
+    fontWeight: "bold",
+    fontSize: 26,
+    lineHeight: 42,
+    marginTop: 8,
+  },
 });
