0348b6c902bdce31239d7cddedf0cf5b5d8e659b
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2021-08-06 17:55:37 -0300

    Aula 3.1

---
+++ src/componentes/Texto.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+export default function Texto({ children, style }) {
+  let estilo = estilos.texto;
+  
+  if(style?.fontWeight === 'bold') {
+    estilo = estilos.textoNegrito;
+  }
+
+  return <Text style={[style, estilo]}>{ children }</Text>
+}
+
+const estilos = StyleSheet.create({
+  texto: {
+    fontFamily: 'MontserratRegular',
+    fontWeight: 'normal',
+  },
+  textoNegrito: {
+    fontFamily: 'MontserratBold',
+    fontWeight: 'normal',
+  }
+});
--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StyleSheet, Image, Dimensions, Text, View } from 'react-native';
+import { StyleSheet, Image, Dimensions, View } from 'react-native';
+
+import Texto from '../componentes/Texto';
 
 import topo from '../../assets/topo.png';
 import logo from '../../assets/logo.png';
@@ -9,16 +11,16 @@ const width = Dimensions.get('screen').width;
 export default function Cesta() {
   return <>
     <Image source={topo} style={estilos.topo} />
-    <Text style={estilos.titulo}>Detalhe da cesta</Text>
+    <Texto style={estilos.titulo}>Detalhe da cesta</Texto>
 
     <View style={estilos.cesta}>
-      <Text style={estilos.nome}>Cesta de Verduras</Text>
+      <Texto style={estilos.nome}>Cesta de Verduras</Texto>
       <View style={estilos.fazenda}>
         <Image source={logo} style={estilos.imagemFazenda} />
-        <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+        <Texto style={estilos.nomeFazenda}>Jenny Jack Farm</Texto>
       </View>
-      <Text style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Text>
-      <Text style={estilos.preco}>R$ 40,00</Text>
+      <Texto style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Texto>
+      <Texto style={estilos.preco}>R$ 40,00</Texto>
     </View>
   </>
 }
@@ -46,7 +48,7 @@ const estilos = StyleSheet.create({
     color: "#464646",
     fontSize: 26,
     lineHeight: 42,
-    fontFamily: "MontserratBold",
+    fontWeight: 'bold',
   },
   fazenda: {
     flexDirection: "row",
@@ -60,7 +62,6 @@ const estilos = StyleSheet.create({
     fontSize: 16,
     lineHeight: 26,
     marginLeft: 12,
-    fontFamily: "MontserratRegular",
   },
   descricao: {
     color: "#A3A3A3",
