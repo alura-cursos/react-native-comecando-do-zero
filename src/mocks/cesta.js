e3b038f8252fe72f31ccbb4b7458a4f7d83cc507
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2021-08-11 19:06:28 -0300

    Aula 4.1

--- src/mocks/cesta.js
+++ src/mocks/cesta.js
@@ -10,6 +10,7 @@ const cesta = {
     nomeFazenda: "Jenny Jack Farm",
     descricao: "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha",
     preco: "R$ 40,00",
+    botao: "Comprar",
   }
 }
 
--- src/telas/Cesta/componentes/Detalhes.js
+++ src/telas/Cesta/componentes/Detalhes.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Image, StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, View, TouchableOpacity } from 'react-native';
 
 import Texto from '../../../componentes/Texto';
 
-export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco }) {
+export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco, botao }) {
   return <>
     <Texto style={estilos.nome}>{ nome }</Texto>
     <View style={estilos.fazenda}>
@@ -12,6 +12,10 @@ export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, pr
     </View>
     <Texto style={estilos.descricao}>{ descricao }</Texto>
     <Texto style={estilos.preco}>{ preco }</Texto>
+
+    <TouchableOpacity style={estilos.botao} onPress={() => {}}>
+      <Texto style={estilos.textoBotao}>{ botao }</Texto>
+    </TouchableOpacity>
   </>
 }
 
@@ -46,5 +50,18 @@ const estilos = StyleSheet.create({
     fontSize: 26,
     lineHeight: 42,
     marginTop: 8,
+  },
+  botao: {
+    marginTop: 16,
+    backgroundColor: "#2A9F85",
+    paddingVertical: 16,
+    borderRadius: 6,
+  },
+  textoBotao: {
+    textAlign: "center",
+    color: "#ffffff",
+    fontSize: 16,
+    lineHeight: 26,
+    fontWeight: "bold",
   },
 })
